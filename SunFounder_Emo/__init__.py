2828fce5089551055b2b8d624d00804962f895ff
john <2556703570@qq.com>
2019-05-23 11:42:33 +0800

    fix bug

--- SunFounder_Emo/__init__.py
+++ SunFounder_Emo/__init__.py
@@ -1,5 +1,5 @@
-from emo import Emo
-from emo_maps import Alphabet, Emotions, Pictures
+from .emo import Emo
+from .emo_maps import Alphabet, Emotions, Pictures
 
 def main():
     print("Test Function to do")
